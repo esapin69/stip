b769bfdba230d2d203397b054eb93d06412ee2ac
esapin69 <eddy.sapin@gmail.com>
2026-09-20 03:31:22 +0200

    Refresh tomorrow hierarchy stylesheet

--- tomorrow-ui.js
+++ tomorrow-ui.js
@@ -1,5 +1,5 @@
 (()=>{"use strict";
-const CSS="tomorrow-hub.css?v=20260920-2";let page=null,float=null,opened=false,scrollLock="",selectedDay="";
+const CSS="tomorrow-hub.css?v=20260920-homehierarchy5";let page=null,float=null,opened=false,scrollLock="",selectedDay="";
 function core(){return window.STIPTomorrow}
 function remote(){return window.STIPTomorrowRemote}
 function notice(msg){try{window.STIPToast?.show?.(msg)}catch{}if(!window.STIPToast?.show)window.alert?.(msg)}
