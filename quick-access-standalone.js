8ccb4feb9eb908f1dd2d957a6c7e990ec2b3c1c1
esapin69 <eddy.sapin@gmail.com>
2026-09-20 20:33:00 +0200

    chore: refresh no-float universal quick access

--- quick-access-standalone.js
+++ quick-access-standalone.js
@@ -1,4 +1,4 @@
 (()=>{'use strict';
 if(document.getElementById('stipQuickUniversal')||document.querySelector('script[data-stip-quick-universal]'))return;
-const s=document.createElement('script');s.src='quick-access-universal.js?v=20260901-favorites1';s.dataset.stipQuickUniversal='1';s.defer=true;document.head.appendChild(s);
+const s=document.createElement('script');s.src='quick-access-universal.js?v=20260920-no-float1';s.dataset.stipQuickUniversal='1';s.defer=true;document.head.appendChild(s);
 })();
