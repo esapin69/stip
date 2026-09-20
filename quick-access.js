6684c2d45398f659d86fa8c5d3e028dc6aa7a732
esapin69 <eddy.sapin@gmail.com>
2026-09-20 23:29:42 +0200

    ui: wire Access artwork into application library

--- quick-access.js
+++ quick-access.js
@@ -274,7 +274,7 @@
     activity:
       '<svg viewBox="0 0 24 24" aria-hidden="true"><path d="M4 19V9M10 19V5M16 19v-7M22 19H2"/></svg>',
     access:
-      '<svg viewBox="0 0 24 24" aria-hidden="true"><rect x="5" y="10" width="14" height="10" rx="2"/><path d="M8 10V7a4 4 0 0 1 8 0v3M12 14v2"/></svg>',
+      '<img src="images/icone_app/access-lock.webp?v=20260920-accesslock1" alt="" aria-hidden="true">',
     newagent:
       '<img src="images/icone_app/nouvel-arrivant-pro.webp?v=20260920-appicons1" alt="" aria-hidden="true">',
     upload:
