7b1c8b0609a8c50e06875cc31d77ec9add94dccc
esapin69 <eddy.sapin@gmail.com>
2026-09-21 23:47:36 +0200

    Bust planning script cache for shift palette

--- stip-loader.js
+++ stip-loader.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
   const loaded = new Map(),
-    V = "20260921-agendapolish1";
+    V = "20260921-shiftboost1";
   function load(src) {
     if (loaded.has(src)) return loaded.get(src);
     const p = new Promise((ok, ko) => {
