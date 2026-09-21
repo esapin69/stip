0c10efa3951c9d3ca0ce4322c15af08341d788c4
esapin69 <eddy.sapin@gmail.com>
2026-09-21 14:10:05 +0200

    chore(loader): refresh subscription and agenda assets

--- stip-loader.js
+++ stip-loader.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
   const loaded = new Map(),
-    V = "20260921-calendarfinal2";
+    V = "20260921-submove2";
   function load(src) {
     if (loaded.has(src)) return loaded.get(src);
     const p = new Promise((ok, ko) => {
