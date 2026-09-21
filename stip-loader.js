8c205733a4911292d1d093c1b600f7a1dfcbc0b3
esapin69 <eddy.sapin@gmail.com>
2026-09-21 17:23:49 +0200

    Refresh dynamically loaded analysis modules

--- stip-loader.js
+++ stip-loader.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
   const loaded = new Map(),
-    V = "20260921-calfeed2";
+    V = "20260921-field2";
   function load(src) {
     if (loaded.has(src)) return loaded.get(src);
     const p = new Promise((ok, ko) => {
