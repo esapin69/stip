67383124e2b6581e5a3611be676c40d3bb6df654
esapin69 <eddy.sapin@gmail.com>
2026-09-21 11:38:16 +0200

    chore(planning): refresh final unified agenda

--- stip-loader.js
+++ stip-loader.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
   const loaded = new Map(),
-    V = "20260921-agentagenda8";
+    V = "20260921-agentagenda9";
   function load(src) {
     if (loaded.has(src)) return loaded.get(src);
     const p = new Promise((ok, ko) => {
