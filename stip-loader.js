3c0bcd40110f29e958056f395dea4fc9ed06f360
esapin69 <eddy.sapin@gmail.com>
2026-09-21 13:30:53 +0200

    chore(calendar): refresh compact calendar assets

--- stip-loader.js
+++ stip-loader.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
   const loaded = new Map(),
-    V = "20260921-weektickets2";
+    V = "20260921-calendarcompact1";
   function load(src) {
     if (loaded.has(src)) return loaded.get(src);
     const p = new Promise((ok, ko) => {
