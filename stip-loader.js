952f716f077be57889e9eb4d2fd4c52db6065c3d
esapin69 <eddy.sapin@gmail.com>
2026-09-21 10:41:35 +0200

    chore(agent): refresh subscription-capable agenda

--- stip-loader.js
+++ stip-loader.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
   const loaded = new Map(),
-    V = "20260921-agentagenda5";
+    V = "20260921-agentagenda6";
   function load(src) {
     if (loaded.has(src)) return loaded.get(src);
     const p = new Promise((ok, ko) => {
