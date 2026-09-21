416fbe558c871cdf192a5dc8ef29862689784247
esapin69 <eddy.sapin@gmail.com>
2026-09-21 11:50:34 +0200

    chore(agent): refresh Monday selection logic

--- stip-loader.js
+++ stip-loader.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
   const loaded = new Map(),
-    V = "20260921-agentagenda9";
+    V = "20260921-agentagenda10";
   function load(src) {
     if (loaded.has(src)) return loaded.get(src);
     const p = new Promise((ok, ko) => {
