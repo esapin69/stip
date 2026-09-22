6c2b675999871aa6aafcf0aac0209dd55fb93dca
esapin69 <eddy.sapin@gmail.com>
2026-09-22 10:09:30 +0200

    Refresh lazy scripts for staffing signals

--- stip-loader.js
+++ stip-loader.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
   const loaded = new Map(),
-    V = "20260921-shiftboost1";
+    V = "20260922-opportunity1";
   function load(src) {
     if (loaded.has(src)) return loaded.get(src);
     const p = new Promise((ok, ko) => {
