828680f87926f650aae6d558d6aee55dd09e52e6
esapin69 <eddy.sapin@gmail.com>
2026-09-22 20:49:35 +0200

    chore(cache): refresh planning special schedule modules

--- stip-loader.js
+++ stip-loader.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
   const loaded = new Map(),
-    V = "20260922-opportunity1";
+    V = "20260922-specialshifts1";
   function load(src) {
     if (loaded.has(src)) return loaded.get(src);
     const p = new Promise((ok, ko) => {
