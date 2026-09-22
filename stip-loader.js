cb06a71871b8be0dfba0b59a3671ef9f10214976
esapin69 <eddy.sapin@gmail.com>
2026-09-22 20:52:14 +0200

    chore(cache): refresh dynamic planning schedule modules

--- stip-loader.js
+++ stip-loader.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
   const loaded = new Map(),
-    V = "20260922-specialshifts1";
+    V = "20260922-specialshifts2";
   function load(src) {
     if (loaded.has(src)) return loaded.get(src);
     const p = new Promise((ok, ko) => {
