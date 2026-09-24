c1d529982e811eb690281fcbd1969859f24ddb62
esapin69 <eddy.sapin@gmail.com>
2026-09-25 00:39:01 +0200

    Navigation: utiliser admin.esapin.com comme site Admin

--- home-shell.js
+++ home-shell.js
@@ -2513,8 +2513,8 @@
     if (k === "newagent")
       return (location.href = "https://esapin69.github.io/Ghe-interne/");
     if (k === "upload")
-      return (location.href = "https://admin-ghe.esapin.com/depot.html");
-    if (k === "admin") return (location.href = "https://admin-ghe.esapin.com/");
+      return (location.href = "https://admin.esapin.com/depot.html");
+    if (k === "admin") return (location.href = "https://admin.esapin.com/");
     if (k === "access") return (location.href = "access-manage.html");
   }
   function panel(v, title = "") {
