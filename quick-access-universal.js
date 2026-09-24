c44184830f1e4fe148706377d3581bfc59684bce
esapin69 <eddy.sapin@gmail.com>
2026-09-24 23:41:29 +0200

    Publish workspace continuity quick-access-universal.js

--- quick-access-universal.js
+++ quick-access-universal.js
@@ -3,7 +3,7 @@
   if (!window.__STIPWorkspaceRequested) {
     window.__STIPWorkspaceRequested = true;
     const workspaceScript = document.createElement("script");
-    workspaceScript.src = "/stip-workspace.js?v=20260924-workspace1";
+    workspaceScript.src = "/stip-workspace.js?v=20260924-workspace2";
     workspaceScript.async = true;
     document.head.appendChild(workspaceScript);
   }
