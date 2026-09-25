cb7fe05821d80dd69b0c14cfb44343b3e7157d9d
esapin69 <eddy.sapin@gmail.com>
2026-09-26 01:24:14 +0200

    Rotate caches after shared navigation rollout

--- stip-sw.js
+++ stip-sw.js
@@ -1,4 +1,4 @@
-const STIP_SW_BUILD="20260926-profile-menu3";
+const STIP_SW_BUILD="20260926-navigation4";
 const STATIC_CACHE="stip-static-"+STIP_SW_BUILD;
 const PAGE_CACHE="stip-pages-"+STIP_SW_BUILD;
 
