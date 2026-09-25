a31f96ab4dce1b3db9c458cfc9dcfe7d3d86f2c5
esapin69 <eddy.sapin@gmail.com>
2026-09-26 01:47:02 +0200

    perf: rotate service worker cache after avatar budget

--- stip-sw.js
+++ stip-sw.js
@@ -1,4 +1,4 @@
-const STIP_SW_BUILD="20260926-network-budget1";
+const STIP_SW_BUILD="20260926-avatar-budget1";
 const STATIC_CACHE="stip-static-"+STIP_SW_BUILD;
 const PAGE_CACHE="stip-pages-"+STIP_SW_BUILD;
 
