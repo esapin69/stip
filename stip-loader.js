7132fe173b36cd76427c8e15297224197c4551ec
esapin69 <eddy.sapin@gmail.com>
2026-09-25 12:23:22 +0200

    Bump lazy asset version for wheelchair DM runtime

--- stip-loader.js
+++ stip-loader.js
@@ -2,7 +2,7 @@
   "use strict";
   const loaded = new Map(),
     loadedStyles = new Map(),
-    V = "20260925-home-perf1";
+    V = "20260925-dm1";
   function load(src) {
     const url = new URL(String(src || ""), document.baseURI);
     url.searchParams.set("v", V);
