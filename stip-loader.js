4d3787eda03d3749bf619fa5cdae1ab6e2167f58
esapin69 <eddy.sapin@gmail.com>
2026-09-26 00:32:32 +0200

    Refresh assets after swipe rollback

--- stip-loader.js
+++ stip-loader.js
@@ -2,7 +2,7 @@
   "use strict";
   const loaded = new Map(),
     loadedStyles = new Map(),
-    V = "20260926-swipe-safe2";
+    V = "20260926-swipe-rollback1";
   function load(src) {
     const url = new URL(String(src || ""), document.baseURI);
     url.searchParams.set("v", V);
