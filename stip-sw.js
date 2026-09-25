b3abe1ca38e85e9198746a265fd6d1fc7e763ccc
esapin69 <eddy.sapin@gmail.com>
2026-09-25 15:16:25 +0200

    chore: rotate STIP static cache

--- stip-sw.js
+++ stip-sw.js
@@ -1,4 +1,4 @@
-const STIP_SW_BUILD="20260925-dm-push1";
+const STIP_SW_BUILD="20260925-wheelchair-flow1";
 const STATIC_CACHE="stip-static-"+STIP_SW_BUILD;
 const PAGE_CACHE="stip-pages-"+STIP_SW_BUILD;
 
