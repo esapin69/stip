05bc6afff87d979d59662985cb69fbf89ef62354
esapin69 <eddy.sapin@gmail.com>
2026-09-25 23:42:51 +0200

    Finalize gesture contract cache version

--- stip-sw.js
+++ stip-sw.js
@@ -1,4 +1,4 @@
-const STIP_SW_BUILD="20260925-gesture-contract1";
+const STIP_SW_BUILD="20260925-gesture-contract2";
 const STATIC_CACHE="stip-static-"+STIP_SW_BUILD;
 const PAGE_CACHE="stip-pages-"+STIP_SW_BUILD;
 
