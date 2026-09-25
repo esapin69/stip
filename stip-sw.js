5b569c1ad220386ec357df86ebf1fee24fefa1d1
esapin69 <eddy.sapin@gmail.com>
2026-09-26 00:43:03 +0200

    Refresh cache after removing horizontal swipes

--- stip-sw.js
+++ stip-sw.js
@@ -1,4 +1,4 @@
-const STIP_SW_BUILD="20260926-swipe-rollback1";
+const STIP_SW_BUILD="20260926-no-horizontal1";
 const STATIC_CACHE="stip-static-"+STIP_SW_BUILD;
 const PAGE_CACHE="stip-pages-"+STIP_SW_BUILD;
 
