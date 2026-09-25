c5b08b18206a05a74df91055175ae41a7b6709da
esapin69 <eddy.sapin@gmail.com>
2026-09-26 01:29:24 +0200

    Rotate caches after final navigation fixes

--- stip-sw.js
+++ stip-sw.js
@@ -1,4 +1,4 @@
-const STIP_SW_BUILD="20260926-navigation5";
+const STIP_SW_BUILD="20260926-navigation6";
 const STATIC_CACHE="stip-static-"+STIP_SW_BUILD;
 const PAGE_CACHE="stip-pages-"+STIP_SW_BUILD;
 
