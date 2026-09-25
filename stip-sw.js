aff8cb486cbc327c400ca6341498d16ee7f411a2
esapin69 <eddy.sapin@gmail.com>
2026-09-26 01:28:19 +0200

    Finalize navigation cache generation

--- stip-sw.js
+++ stip-sw.js
@@ -1,4 +1,4 @@
-const STIP_SW_BUILD="20260926-navigation4";
+const STIP_SW_BUILD="20260926-navigation5";
 const STATIC_CACHE="stip-static-"+STIP_SW_BUILD;
 const PAGE_CACHE="stip-pages-"+STIP_SW_BUILD;
 
