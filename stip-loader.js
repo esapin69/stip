24f9ea26e47bd71c0c62fb49011611f36b196a4e
esapin69 <eddy.sapin@gmail.com>
2026-09-25 15:15:31 +0200

    chore: bump Chat STIP DM runtime assets

--- stip-loader.js
+++ stip-loader.js
@@ -2,7 +2,7 @@
   "use strict";
   const loaded = new Map(),
     loadedStyles = new Map(),
-    V = "20260925-dm1";
+    V = "20260925-dm-push1";
   function load(src) {
     const url = new URL(String(src || ""), document.baseURI);
     url.searchParams.set("v", V);
