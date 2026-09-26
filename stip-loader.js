b350f5824cb26c0da6e986c785c0a03768705569
esapin69 <eddy.sapin@gmail.com>
2026-09-26 03:25:10 +0200

    perf(chat): cache refresh after incremental DM

--- stip-loader.js
+++ stip-loader.js
@@ -2,7 +2,7 @@
   "use strict";
   const loaded = new Map(),
     loadedStyles = new Map(),
-    V = "20260926-chat-budget2";
+    V = "20260926-chat-budget3";
   function load(src) {
     const url = new URL(String(src || ""), document.baseURI);
     url.searchParams.set("v", V);
