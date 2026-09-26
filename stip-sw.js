f1bb1b97940151feceb86b28ee569ea54eab4628
esapin69 <eddy.sapin@gmail.com>
2026-09-26 11:00:33 +0200

    chore(cache): activate no-iframe page architecture

--- stip-sw.js
+++ stip-sw.js
@@ -1,4 +1,4 @@
-const STIP_SW_BUILD="20260926-shared-header-scroll1";
+const STIP_SW_BUILD="20260926-no-iframe-pages1";
 const STATIC_CACHE="stip-static-"+STIP_SW_BUILD;
 const PAGE_CACHE="stip-pages-"+STIP_SW_BUILD;
 
