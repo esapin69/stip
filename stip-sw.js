095d3fd2e51509fcab75e08412ab1b9585a1d493
esapin69 <eddy.sapin@gmail.com>
2026-09-26 09:41:14 +0200

    chore(cache): activate Responsable scroll repair

--- stip-sw.js
+++ stip-sw.js
@@ -1,4 +1,4 @@
-const STIP_SW_BUILD="20260926-team-unified-scroll1";
+const STIP_SW_BUILD="20260926-responsable-unified-scroll1";
 const STATIC_CACHE="stip-static-"+STIP_SW_BUILD;
 const PAGE_CACHE="stip-pages-"+STIP_SW_BUILD;
 
