5e2c10566b7241047594dacc7c6941b088c6beba
esapin69 <eddy.sapin@gmail.com>
2026-09-26 03:55:16 +0200

    chore(cache): invalidate pages after places recovery

--- stip-sw.js
+++ stip-sw.js
@@ -1,4 +1,4 @@
-const STIP_SW_BUILD="20260926-iframe-scroll2";
+const STIP_SW_BUILD="20260926-places-recovery1";
 const STATIC_CACHE="stip-static-"+STIP_SW_BUILD;
 const PAGE_CACHE="stip-pages-"+STIP_SW_BUILD;
 
